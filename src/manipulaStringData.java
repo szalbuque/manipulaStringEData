9354a1189cb993352e3ee39149ed0f2a97ac38b0
Silvia Rocha <szalbuque@gmail.com>
2022-09-11 08:09:14 -0300

    finalizado

--- src/manipulaStringData.java
+++ src/manipulaStringData.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class manipulaStringData {
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
+public class manipulaStringData {
+    public static void main(String[] args) {
+        String nome = "Silvia";
+        // padrão de data ISO 8601
+        LocalDate hoje = LocalDate.now();
+        Locale brasil = new Locale("pt", "BR");
+        String diaSemana = hoje.getDayOfWeek().getDisplayName(TextStyle.FULL,brasil);
+        LocalDateTime agora = LocalDateTime.now();
+        String saudacao = " ";
+        if (agora.getHour() >= 0 && agora.getHour() < 12) {
+            saudacao = "Bom dia";
+        } else if (agora.getHour() >= 12 && agora.getHour() < 18) {
+            saudacao = "Boa tarde";
+        } else {
+            saudacao = "Boa noite";
+        }
+        System.out.printf("Ola, %s. Hoje é %s, %s.%n", nome, diaSemana, saudacao.toUpperCase());
+    }
 }
